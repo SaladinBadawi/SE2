66cd108890135d330aeff7b176547cdd713646e5
Salaheddin Badawi <salahbadawi12@gmail.com>
2020-02-29 12:27:31 +0200

    first test

---
+++ Calcu/src/cal/CalculatorTest2.java
@@ -0,0 +1,24 @@
+package cal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest2 {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSub() {
+		fail("Not yet implemented");
+	}
+
+}
