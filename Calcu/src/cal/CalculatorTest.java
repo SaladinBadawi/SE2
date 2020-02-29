ea4ec2391729455b355b3ab9ae0a23d9839c5a47
Salaheddin Badawi <salahbadawi12@gmail.com>
2020-02-29 12:25:41 +0200

    v2.1

--- Calcu/src/cal/CalculatorTest.java
+++ Calcu/src/cal/CalculatorTest.java
@@ -1,34 +1,43 @@
 package cal;
-import org.junit.Assert;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
 
 
-public class CalculatorTest extends TestCase {
+public class CalculatorTest {
+	
 	Calculator ca =new Calculator();
+	@Test
 	public void testAdd_BothNumbersArePositive_ShouldReturnPositiveNumber(){
 		int first=1;
 		int second=2;
 		
 		int result = ca.add(first,second);
-		Assert.assertTrue(result>0);
+		assertTrue(result>0);
 		//fail("Not yet implemented");
 	}
+	@Test
 	public void testAdd_BothNumbersAreNegative_ShouldReturnNegativeNumber(){
 		int first=-1;
 		int second=-2;
 		int result = ca.add(first,second);
-		Assert.assertTrue(result<0);
+		assertTrue(result<0);
 	}
+	@Test
 	public void testAdd_GreaterNumbersISPositiveOtherOneIsNegative_ShouldReturnPositiveNumbedr() {
 		int first=-1;
 		int second=2;
 		int result = ca.add(first,second);
-		Assert.assertTrue(result>0);
+		assertTrue(result>0);
 	}
+	@Test
 	public void testAdd_GreaterNumbersIsNegativeOtherOneISPositive_ShouldReturnNegativeNumber() {
 		int first=1;
 		int second=-2;
 		int result = ca.add(first,second);
-		Assert.assertTrue(result<0);
+		assertTrue(result<0);
 	}
 	//test
 
